c33e21c2a1eca491a8c89d9f650eeae34c05ab5f
jaimemin <jaimemin@gmail.com>
2022-07-24 09:56:20 +0900

    Initial commit

---
+++ app/src/main/java/com/tistory/jaimemin/androidpractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tistory.jaimemin.androidpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
