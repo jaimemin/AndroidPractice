6496f84dc0d9fa146ac0c37835c1a96e78065a42
jaimemin <jaimemin@gmail.com>
2022-08-03 11:28:10 +0900

    learned about ApplicationContext

---
+++ app/src/main/java/com/tistory/jaimemin/androidpractice/ApplicationActivityTwo.kt
@@ -0,0 +1,11 @@
+package com.tistory.jaimemin.androidpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ApplicationActivityTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_application_two)
+    }
+}
