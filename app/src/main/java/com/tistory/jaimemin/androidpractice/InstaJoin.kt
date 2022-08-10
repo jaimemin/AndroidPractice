3c14bd98fb5da32d6369cb092cd7871b119b05eb
jaimemin <jaimemin@gmail.com>
2022-08-10 23:44:54 +0900

    implemented insta clone app login page

---
+++ app/src/main/java/com/tistory/jaimemin/androidpractice/InstaJoin.kt
@@ -0,0 +1,11 @@
+package com.tistory.jaimemin.androidpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InstaJoin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insta_join)
+    }
+}
